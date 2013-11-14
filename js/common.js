fa4898e40899f5d635c87202ab97ae7ee2bae9a7
dpismenny <denis.pismenny@gmail.com>
2013-11-14 21:17:47 +0200

    hero slider

--- js/common.js
+++ js/common.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
 
 	$('input, textarea').placeholder();
 
+	// tabs
+	$(".js-tabs-pane").removeClass("is-visible");
+	$(".js-tabs-nav li:first").addClass("is-active");
+	$(".js-tabs-pane:first").addClass("is-visible");
+
+	$(".js-tabs-nav li").click(function() {
+		$(".js-tabs-nav li").removeClass("is-active");
+		$(this).addClass("is-active");
+		$(".js-tabs-pane").removeClass("is-visible");
+		var activeTab = $(this).find("a").attr("href");
+		$(activeTab).addClass("is-visible");
+		return false;
+	});
+
 });
+
+
+
+
+
