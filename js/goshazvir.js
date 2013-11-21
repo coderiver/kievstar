293fe01572ab1b50bda3ccb71409a0410f05aa86
akella <akella@Yuris-MacBook-Pro.local>
2013-11-21 10:50:57 +0200

    merge all except main.sass

---
+++ js/goshazvir.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+	//select
+	function select() {
+		var el = $('.js-select-zvir');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.bind("click",function(event){
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$(this).parent().addClass('is-open');
+			};
+			event.stopPropagation();
+		});
+		checkbox.bind("click",function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			$(this).toggleClass("is-selected");
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select-zvir").find(".js-select-text").text(text);
+			$(this).parents(".js-select-zvir").find(".js-select-input").val(id);
+	 });
+	};
+	select();
+
+	//click document
+	$(document).click(function() {
+		$('.js-select-zvir').removeClass('is-open');
+	});
+
+	$(function() {
+	  $( "#slider-range-min" ).slider({
+	    range: "min",
+	    value: 173,
+	    min: 0,
+	    max: 1000,
+	    slide: function( event, ui ) {
+	      $( "#amount" ).val( "$" + ui.value );
+	    }
+	  });
+	  $( "#amount" ).val( "$" + $( "#slider-range-min" ).slider( "value" ) );
+	});
+});
