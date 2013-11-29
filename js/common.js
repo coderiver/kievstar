94ee4f902437c40da336f2f9c006d578ab1b823f
akella <akella@Yuris-MacBook-Pro.local>
2013-11-29 19:12:09 +0200

    fix cabinet ie7

--- js/common.js
+++ js/common.js
@@ -66,4 +66,98 @@ $(document).ready(function() {
 		precision : true
 	});
 
+
+
+
+
+
+	// popup
+	jQuery('.profile__login').click(function(event) {
+		jQuery('.popup').addClass('is-active');
+		jQuery('.overlay').addClass('is-active');
+		return false;
+	});
+	jQuery('.popup__close, .overlay').click(function(event) {
+		jQuery('.popup').removeClass('is-active');
+		jQuery('.overlay').removeClass('is-active');
+		return false;
+	});
+
+		
+	//select
+	function select() {
+		var el = $('.js-select-other');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.bind("click",function(event){
+			tooltip();
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+				$(this).parent().parent().parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$('.row').removeClass('is-open')
+				$(this).parent().addClass('is-open');
+				$(this).parent().parent().parent().addClass('is-open');
+			};
+			event.stopPropagation();
+		});
+		checkbox.bind("click",function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			$(this).toggleClass("is-selected");
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select-other").find(".js-select-text").text(text);
+			$(this).parents(".js-select-other").find(".js-select-input").val(id);
+	 });
+	};
+	select();
+
+	//click document
+	$(document).click(function() {
+		$('.js-select-other').removeClass('is-open');
+	});
+
+	// for tag
+	$('.tag .icon_cross').click(function() {
+	  $(this).parent().fadeOut(100);
+	})
+
+	$(function() {
+	  $( "#slider-range-min" ).slider({
+	    range: "min",
+	    value: 173,
+	    min: 0,
+	    max: 1000,
+	    slide: function( event, ui ) {
+	      $( "#amount" ).val(ui.value + " грн");
+	    }
+	  });
+	  $( "#amount" ).val($( "#slider-range-min" ).slider( "value" ) + " грн");
+	});
+
+
+	function tooltip() {
+		var btn = $('.js-tooltip');
+		var box = $('.js-tooltip-box');
+		var box_content = box.find('.box__content');		
+		btn.bind('click', function(){
+			var text = $(this).attr('data-tooltip');
+			box_content.html(text);
+			//slide
+			var pos_top = $(this).position().top;
+			box.css('top', pos_top);
+		});
+	}
+	tooltip();
+
+
+
 });
